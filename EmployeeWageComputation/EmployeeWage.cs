5ca678a422ce5643660768d2cce41d10557eec2f
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-04-07 14:06:17 +0530

    [VINAY] Add: Emloyee Salary Maximum Working Hours as 100

--- EmployeeWageComputation/EmployeeWage.cs
+++ EmployeeWageComputation/EmployeeWage.cs
@@ -15,40 +15,44 @@ namespace EmployeeWageComputation
         const int EMP_FULL_DAY_WRKING_HR = 8;
         const int EMP_PART_TIME_WRKING_HR = 4;
         const int EMP_WORKING_PR_MONTH= 20;
+        const int MAX_WORKING_HR = 100;
          
        
         //variable values
         int empHrs = 0;
         int totalEmpSalary=0;
-        
+        int totalEmpHrs = 0;
+        int day = 0;
       
         public void monthlyEmpWage()
         {
-            for (int day = 0; day <= EMP_WORKING_PR_MONTH; day++)
+            while (day <= EMP_WORKING_PR_MONTH && empHrs <= MAX_WORKING_HR)
             {
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
-
+                
                 switch (empCheck)
                 {
                     case FULL_TIME_EMP:
-                       
+
                         empHrs += EMP_FULL_DAY_WRKING_HR;
                         break;
                     case PART_TIME_EMP:
-                       
+
                         empHrs += EMP_PART_TIME_WRKING_HR;
                         break;
                     default:
                         empHrs = 0;
                         break;
                 }
-                 
+                day++;
+
             }
+
             totalEmpSalary = empHrs * EMP_WAGE_PR_HR;
-            Console.WriteLine("One Month Employee salary is :" + totalEmpSalary);
+            Console.WriteLine("One month Employee Salary is :" + totalEmpSalary);
         }
-            
-        
+       
+
     }
 }
