0297946510cb1239f4d88be16b9a6952050aab42
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-04-19 13:14:34 +0530

    [VINAY] Add:Calculating Employee Salary With the Help of Passing Parameter

--- EmployeeWageComputation/EmployeeWage.cs
+++ EmployeeWageComputation/EmployeeWage.cs
@@ -11,22 +11,15 @@ namespace EmployeeWageComputation
         //constance value
         const int PART_TIME_EMP = 1;
         const int FULL_TIME_EMP = 2;
-        const int EMP_WAGE_PR_HR = 20;
         const int EMP_FULL_DAY_WRKING_HR = 8;
         const int EMP_PART_TIME_WRKING_HR = 4;
-        const int EMP_WORKING_PR_MONTH= 20;
-        const int MAX_WORKING_HR = 100;
-         
-       
+            
         //variable values
-        int empHrs = 0;
-        int totalEmpSalary=0;
-        int totalEmpHrs = 0;
-        int day = 0;
-      
-        public void monthlyEmpWage()
+        int empHrs = 0, totalEmpSalary = 0, totalEmpHrs = 0, day = 0;
+             
+        public void monthlyEmpWage(string company_Name,int EMP_WAGE_PR_HR,int MAX_WORKING_HR,int EMP_WORKING_Days_PR_MONTH)
         {
-            while (day <= EMP_WORKING_PR_MONTH && empHrs <= MAX_WORKING_HR)
+            while (day <= EMP_WORKING_Days_PR_MONTH && empHrs <= MAX_WORKING_HR)
             {
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -5,7 +5,15 @@ class Program
     public static void Main(String[] args)
     {
         EmployeeWage computation = new EmployeeWage();
-        computation.monthlyEmpWage();
+        Console.WriteLine("Enter the Compnay Name");
+        string comp_Name=Console .ReadLine();
+        Console.WriteLine("Enter the Employee Salary Pr Hours");
+        int emp_Wage_Pr_Hours=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the Maximum Working Hours Of That Company");
+        int max_Working_Hour = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the Working Days in One Month");
+        int working_Days_In_One_Month = Convert.ToInt32(Console.ReadLine());
+        computation.monthlyEmpWage(comp_Name, emp_Wage_Pr_Hours, max_Working_Hour, working_Days_In_One_Month);
     }
 }
 
