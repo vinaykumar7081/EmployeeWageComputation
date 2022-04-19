767d52d0dcced117f4456aca475b68c278eb1a6b
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-04-19 12:17:20 +0530

    [VINAY] Add: Calculating Employee Wages With the Help of Class And Methods

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -1,3 +1,11 @@
-﻿using EmployeeWageComputation;
-EmployeeWage computation=new EmployeeWage();
-computation.monthlyEmpWage();
+﻿using System;
+using EmployeeWageComputation;
+class Program
+{
+    public static void Main(String[] args)
+    {
+        EmployeeWage computation = new EmployeeWage();
+        computation.monthlyEmpWage();
+    }
+}
+
